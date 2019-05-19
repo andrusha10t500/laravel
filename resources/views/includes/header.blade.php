9f1590a9ebeac26fd8307d5049df45f5ef58e7de
andrusha10t500 <andrusha10t500@gmail.com>
2019-05-19 17:36:27 +1000

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">Brand</a>
+
+
+        <form class="form-inline">
+            <a class="btn btn-outline-success my-2 my-sm-0" href="{{route('account')}}">Account</a>
+            <a class="btn btn-outline-success my-2 my-sm-0" href="{{route('logout')}}">Logout</a>
+        </form>
+    </nav>
+</header>
